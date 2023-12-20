c76d89c097aa20072f7d1bb148f825fd99efd4a8
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2023-12-20 18:14:02 +0800

    ADD FILE

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -214,7 +214,7 @@ export default () => {
         when={!loading()}
         fallback={() => (
           <div class="gen-cb-wrapper">
-            <span>AI is thinking...</span>
+            <span>人工智能正在思考...</span>
             <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
           </div>
         )}
