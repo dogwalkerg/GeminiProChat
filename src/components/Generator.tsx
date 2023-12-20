01b1b414c485ec8666176f53f151db49f499941f
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2023-12-20 18:00:35 +0800

    add file

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -223,7 +223,7 @@ export default () => {
           <textarea
             ref={inputRef!}
             onKeyDown={handleKeydown}
-            placeholder="Enter something..."
+            placeholder="请输入您的问题..."
             autocomplete="off"
             autofocus
             onInput={() => {
